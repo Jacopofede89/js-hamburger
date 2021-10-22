b1587ff4bff03d6c1f6c82526c22a6af90c7b8bf
Jacopofede89 <jacopofede89@gmail.com>
2021-10-22 15:55:22 +0200

    sviluppo

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuRight = document.getElementById("hamburger-menu");
+
+menuRight.addEventListener("click",
+     
+   function(){
+       
+    document.getElementById("hamburger-menu").style.display = "block"
+}
+);
